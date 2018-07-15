08229eec229f916cf4a2e1c343bc7fdf9ab6e35e
Jo-R <jo.rigg8@gmail.com>
2018-07-15 09:56:56 +0100

    added parentHomeActivity and linked up menu to start it

--- app/src/main/java/com/jorigg/android/chatbox/HomeActivity.java
+++ app/src/main/java/com/jorigg/android/chatbox/HomeActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -51,6 +52,19 @@ public class HomeActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.activity_parent_menu, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.parent_mode:
+                Intent intent = new Intent(HomeActivity.this, ParentHomeActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 
     private void populateSpinner() {
