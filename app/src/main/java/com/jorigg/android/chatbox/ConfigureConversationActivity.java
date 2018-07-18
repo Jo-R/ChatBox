8e63fec879ae5b559b01a9c13cec2cb62ffbc986
Jo-R <jo.rigg8@gmail.com>
2018-07-18 15:30:48 +0100

    config screen element spinner populated

--- app/src/main/java/com/jorigg/android/chatbox/ConfigureConversationActivity.java
+++ app/src/main/java/com/jorigg/android/chatbox/ConfigureConversationActivity.java
@@ -4,10 +4,17 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 
+import com.jorigg.android.chatbox.model.AskForSomething;
 import com.jorigg.android.chatbox.model.ChatBank;
+import com.jorigg.android.chatbox.model.ConversationElementEnum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ConfigureConversationActivity extends AppCompatActivity {
 
@@ -16,6 +23,8 @@ public class ConfigureConversationActivity extends AppCompatActivity {
     ImageButton mAddTitleButton;
     EditText mAddTitleField;
 
+    Spinner mSelectElementSpinner;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +32,7 @@ public class ConfigureConversationActivity extends AppCompatActivity {
 
         mChatBank = ChatBank.get(this);
 
+        //TODO shuld this show existing if eists and how would that be editable???
         mAddTitleField = findViewById(R.id.config_edit_title);
         mAddTitleButton= findViewById(R.id.config_edit_title_button);
         mAddTitleButton.setOnClickListener(new View.OnClickListener() {
@@ -31,5 +41,24 @@ public class ConfigureConversationActivity extends AppCompatActivity {
                 mChatBank.addNewConversation(mAddTitleField.getText().toString(), "Ask for Something");
             }
         });
+
+        mSelectElementSpinner = findViewById(R.id.config_elements_spinner);
+        populateElementSpinner();
+    }
+
+    private void populateElementSpinner() {
+        ArrayList<String> elements = new ArrayList<>();
+
+        //TODO is hard coded for ask for something
+        AskForSomething.AskForSomethingElements[] values = AskForSomething.AskForSomethingElements
+                .values();
+        for (AskForSomething.AskForSomethingElements element : values) {
+            elements.add(element.name());
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout
+                .simple_spinner_item, elements);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mSelectElementSpinner.setAdapter(adapter);
     }
 }
