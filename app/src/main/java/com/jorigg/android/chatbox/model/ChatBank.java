4c3fc96b78ec8b0c05bd875ec9c2325cec1f9a90
Jo-R <jo.rigg8@gmail.com>
2018-07-25 11:19:20 +0100

    remove hard coded conversation adding from CB

--- app/src/main/java/com/jorigg/android/chatbox/model/ChatBank.java
+++ app/src/main/java/com/jorigg/android/chatbox/model/ChatBank.java
@@ -23,7 +23,6 @@ public class ChatBank {
     private ChatBank(Context context) {
         mContext = context.getApplicationContext();
         mConversationLibrary = new ArrayList<>();
-//        addConversation();
     }
 
     public ArrayList<String> getChatTitles() {
@@ -46,41 +45,6 @@ public class ChatBank {
         return mConversationLibrary;
     }
 
-//    public void addConversation() { //TODO HARD CODED FOR NOW
-//       //add a new conversation - create it and add to the library
-//        Conversation testConvo = new AskForSomething("test");
-//        testConvo.addToConversation(AskForSomething.AskForSomethingElements.GREETING,"Hi");
-//        testConvo.addToConversation(AskForSomething.AskForSomethingElements.ALT_GREETING,"Hi there dude");
-//        testConvo.addToConversation(AskForSomething.AskForSomethingElements.RTN_GREETING, "Yo");
-//        testConvo.addToConversation(AskForSomething.AskForSomethingElements.RTN_GREETING, "Big up");
-//        testConvo.addToConversation(AskForSomething.AskForSomethingElements.MAKE_REQUEST, "Please " +
-//                "can I have a thing?");
-//        testConvo.addToConversation(AskForSomething.AskForSomethingElements.ALT_MAKE_REQUEST,
-//                "Thing");
-//        testConvo.addToConversation(AskForSomething.AskForSomethingElements.AGREE_REQUEST, "Of course");
-//        testConvo.addToConversation(AskForSomething.AskForSomethingElements.AGREE_REQUEST, "Yes," +
-//                " here you are");
-//        testConvo.addToConversation(AskForSomething.AskForSomethingElements.REQ_CLARIFY, "Sorry, " +
-//                "can you just say that again?");
-//        testConvo.addToConversation(AskForSomething.AskForSomethingElements.PROVIDE_CLARIFY, "Of course, " +
-//                "please can I have a thing?");
-//        testConvo.addToConversation(AskForSomething.AskForSomethingElements.ALT_PROVIDE_CLARIFY,
-//                "Thing");
-//        testConvo.addToConversation(AskForSomething.AskForSomethingElements.REFUSE_REQ, "Sorry, no");
-//        testConvo.addToConversation(AskForSomething.AskForSomethingElements.REFUSE_REQ, "No, not now");
-//        testConvo.addToConversation(AskForSomething.AskForSomethingElements.ACKNOWL_REFUSAL, "OK, no problem");
-//        testConvo.addToConversation(AskForSomething.AskForSomethingElements.ACKNOWL_REFUSAL, "Alright then");
-//        testConvo.addToConversation(AskForSomething.AskForSomethingElements.THANK, "Thanks very " +
-//                "much");
-//        testConvo.addToConversation(AskForSomething.AskForSomethingElements.THANK, "Ta");
-//        testConvo.addToConversation(AskForSomething.AskForSomethingElements.ACKNOWL_THANK, "No problem");
-//
-//        mConversationLibrary.add(testConvo);
-//
-//        Conversation dummy = new AskForSomething("empty convo");
-//        mConversationLibrary.add(dummy);
-//
-//    }
 
     public Conversation getConversation(String title) {
         for ( Conversation conversation : mConversationLibrary) {
