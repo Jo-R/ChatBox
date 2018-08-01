49e42644e2610e26780d6c0d653caa1c9190bc60
Jo-R <jo.rigg8@gmail.com>
2018-08-01 14:14:38 +0100

    classes added for unit tests

---
+++ app/src/test/java/com/jorigg/android/chatbox/model/ChatBankTest.java
@@ -0,0 +1,37 @@
+package com.jorigg.android.chatbox.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ChatBankTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void get() {
+    }
+
+    @Test
+    public void getChatTitles() {
+    }
+
+    @Test
+    public void addNewConversation() {
+    }
+
+    @Test
+    public void getConversationLibrary() {
+    }
+
+    @Test
+    public void getConversation() {
+    }
+
+    @Test
+    public void deleteConversation() {
+    }
+}
---
+++ app/src/test/java/com/jorigg/android/chatbox/model/GreetingTest.java
@@ -0,0 +1,12 @@
+package com.jorigg.android.chatbox.model;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class GreetingTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+}
