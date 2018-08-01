270ecf131baaac77302dc51a8997c64f13533a2d
Jo-R <jo.rigg8@gmail.com>
2018-08-01 14:10:08 +0100

    hamcrest and mockito added as depenencies - test class for ask for something created

---
+++ app/src/test/java/com/jorigg/android/chatbox/model/AskForSomethingTest.java
@@ -0,0 +1,13 @@
+package com.jorigg.android.chatbox.model;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class AskForSomethingTest {
+
+    @Before
+    public void setUp() throws Exception {
+        AskForSomething afsTest = new AskForSomething("test title");
+    }
+}
