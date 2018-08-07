23dadb59d0bb6af255d62df9c98f090f2e042580
Jo-R <jo.rigg8@gmail.com>
2018-08-07 13:53:20 +0100

    started chatBank unit test

--- app/src/test/java/com/jorigg/android/chatbox/model/ChatBankTest.java
+++ app/src/test/java/com/jorigg/android/chatbox/model/ChatBankTest.java
@@ -1,14 +1,27 @@
 package com.jorigg.android.chatbox.model;
 
+import android.content.Context;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class ChatBankTest {
+
+    @Mock
+    Context mockContext;
+
+    private ChatBank mSubject;
 
     @Before
     public void setUp() throws Exception {
+        mSubject = ChatBank.get(mockContext);
+
     }
 
     @Test
