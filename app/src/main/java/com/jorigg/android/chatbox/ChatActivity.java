8cdea915a2f3d1e943b45bb9bd9bfe381d4b0f92
Jo-R <jo.rigg8@gmail.com>
2018-08-08 11:43:11 +0100

    minor change - end of session

--- app/src/main/java/com/jorigg/android/chatbox/ChatActivity.java
+++ app/src/main/java/com/jorigg/android/chatbox/ChatActivity.java
@@ -2,6 +2,7 @@ package com.jorigg.android.chatbox;
 
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Handler;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
